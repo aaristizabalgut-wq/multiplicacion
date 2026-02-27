d0b75f6bbccea69cc1156d8b1ab09673492a12e5
Ana <aaristizabalgut@gmail.com>
2026-02-27 16:15:29 -0500

    Añadir lo que no quedo

---
+++ multiplicacion/Program.cs
@@ -0,0 +1,16 @@
+﻿class TablaMultiplicacion
+{
+    static async Task Main(string[] args)
+    {
+        Console.WriteLine("MULTIPLICACION");
+        Console.WriteLine("Digite el numero de la tabla que desea consultar...");
+        int numeroTabla = Convert.ToInt32(Console.ReadLine());
+        Console.WriteLine($"========== TABLA DEL {numeroTabla}: ===========");
+        await Task.Delay(1000);
+        for (int i = 1; i <= 10; i++)
+        {
+           
+            Console.WriteLine($"{numeroTabla} * {i} : {numeroTabla * i}");
+        }  Console.ReadKey();
+    }
+}
